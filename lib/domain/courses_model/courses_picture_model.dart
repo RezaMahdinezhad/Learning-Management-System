a33be9a0a3180c4f5a01fc801bb47afbcf6d8feb
RezaMahdinezhad <RezaMahdinezhad@gmail.com>
2023-03-11 14:19:38 +0330

    adding signin page

---
+++ lib/domain/courses_model/courses_picture_model.dart
@@ -0,0 +1,21 @@
+class CoursesPictureModel {
+  final String image;
+  CoursesPictureModel(this.image);
+
+  static final List<CoursesPictureModel> items = [
+    CoursesPictureModel(
+        'assets/image/courses/06c727a0c75eb75b20ff1c6d3f272dfcc5991f57_1659358030.jpeg'),
+    CoursesPictureModel(
+        'assets/image/courses/12c02a191f935cec2de86bcbb852ffd1023c97ec_1659525362.jpeg'),
+    CoursesPictureModel(
+        'assets/image/courses/30d52d0d620f083aab282cbd348aa6377f3d0763_1659765671.jpeg'),
+    CoursesPictureModel(
+        'assets/image/courses/78aa2fd837d9983398999aab3e47d14ac4c1c0b2_1659863150.jpeg'),
+    CoursesPictureModel(
+        'assets/image/courses/7232c023e58b9d7c7d04b8c40f1112757842d56f_1660158347.jpeg'),
+    CoursesPictureModel(
+        'assets/image/courses/f36ecac90cff5d33cc79bb703b69d09d29cb5ee0_1659436771.jpeg'),
+    CoursesPictureModel(
+        'assets/image/courses/ffe77dcb1f7603dde4492004165ca8e2c3c8457f_1660056383.jpeg'),
+  ];
+}
--- lib/presentation/widgets/navigationBar_widgets/account.dart
+++ lib/presentation/widgets/navigationBar_widgets/account.dart
@@ -1,63 +1,253 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:learning_management_system/presentation/authentication/sign_up/sign_up_screen.dart';
 import 'package:learning_management_system/presentation/resources/color_manager.dart';
 
 import '../../resources/route_manager.dart';
 
+// class Account extends StatelessWidget {
+//   const Account({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: PreferredSize(
+//         preferredSize: const Size.fromHeight(kToolbarHeight),
+//         child: Container(
+//           decoration: const BoxDecoration(
+//             boxShadow: [
+//               BoxShadow(
+//                 color: Colors.white,
+//                 offset: Offset(0, 1.0),
+//               )
+//             ]
+//           ),
+//           child: AppBar(
+//             title: Text(
+//               'Account',
+//               style: Theme.of(context).appBarTheme.titleTextStyle,
+//             ),
+//             centerTitle: true,
+//             backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+//           ),
+//         ),
+//       ),
+//       backgroundColor: ColorManager.black,
+//       body: TextButton(
+//           onPressed: (){
+//             // Navigator.of(context).push(PageRouteBuilder(
+//             //     pageBuilder: (context,animation,anotherAnimation){
+//             //       return SignUpScreen();
+//             //     },
+//             //   transitionDuration: Duration(milliseconds: 600),
+//             //   transitionsBuilder: (context,animation,anotherAnimation,child){
+//             //       animation = CurvedAnimation(
+//             //           parent: animation,
+//             //           curve: Curves.linear);
+//             //       return SlideTransition(
+//             //         position: Tween<Offset>(
+//             //           begin: const Offset(1,0),
+//             //           end: Offset.zero,
+//             //         ).animate(animation),
+//             //         child: child,
+//             //       );
+//             //   }
+//             // ));
+//             Navigator.pushNamed(context, Routes.signUp);
+//           },
+//           child: Text(
+//             'Account',
+//             style: Theme.of(context).textTheme.bodyText1,
+//           ),
+//       ),
+//     );
+//   }
+// }
+
 class Account extends StatelessWidget {
   const Account({Key? key}) : super(key: key);
+
+  Widget Terms() {
+    return TextButton(onPressed: () {}, child: Text('Terms'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: Container(
-          decoration: const BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white,
-                offset: Offset(0, 1.0),
-              )
-            ]
-          ),
-          child: AppBar(
-            title: Text(
-              'Account',
-              style: Theme.of(context).appBarTheme.titleTextStyle,
-            ),
-            centerTitle: true,
-            backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'By using our services you are agreeing to our ',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Terms',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: Text(
+                      'and',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  Text(
+                    'Privacy Statement',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(width: 1.0),
+                    padding: EdgeInsets.all(8),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FaIcon(FontAwesomeIcons.yahoo),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Sign in with email',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(width: 1.0),
+                    padding: EdgeInsets.all(8),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FaIcon(FontAwesomeIcons.google),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Sign in with Google',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(width: 1.0),
+                    padding: EdgeInsets.all(8),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FaIcon(FontAwesomeIcons.facebook),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Sign in with Facebook',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(width: 1.0),
+                    padding: EdgeInsets.all(8),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FaIcon(FontAwesomeIcons.apple),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Sign in with Apple',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 250,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'New here?',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Create an account',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
-      ),
-      backgroundColor: ColorManager.black,
-      body: TextButton(
-          onPressed: (){
-            // Navigator.of(context).push(PageRouteBuilder(
-            //     pageBuilder: (context,animation,anotherAnimation){
-            //       return SignUpScreen();
-            //     },
-            //   transitionDuration: Duration(milliseconds: 600),
-            //   transitionsBuilder: (context,animation,anotherAnimation,child){
-            //       animation = CurvedAnimation(
-            //           parent: animation,
-            //           curve: Curves.linear);
-            //       return SlideTransition(
-            //         position: Tween<Offset>(
-            //           begin: const Offset(1,0),
-            //           end: Offset.zero,
-            //         ).animate(animation),
-            //         child: child,
-            //       );
-            //   }
-            // ));
-            Navigator.pushNamed(context, Routes.signUp);
-          },
-          child: Text(
-            'Account',
-            style: Theme.of(context).textTheme.bodyText1,
-          ),
       ),
     );
   }
